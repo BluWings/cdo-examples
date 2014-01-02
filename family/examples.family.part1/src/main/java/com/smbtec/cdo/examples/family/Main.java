6863ceff01f7104bdda3d24c9f4f01b3cb78059b
Lars Martin <lars.martin@smb-tec.com>
2014-01-02 10:20:42 +0100

    - simplified first example - moved be-directional relationships to next part

--- family/examples.family.part1/src/main/java/com/smbtec/cdo/examples/family/Main.java
+++ family/examples.family.part1/src/main/java/com/smbtec/cdo/examples/family/Main.java
@@ -10,7 +10,6 @@ import org.slf4j.LoggerFactory;
 
 import com.buschmais.cdo.api.CdoManager;
 import com.buschmais.cdo.api.CdoManagerFactory;
-import com.buschmais.cdo.api.Query;
 import com.buschmais.cdo.api.Query.Result;
 import com.buschmais.cdo.api.Query.Result.CompositeRowObject;
 import com.buschmais.cdo.api.TransactionAttribute;
@@ -26,45 +25,58 @@ public class Main {
 
 	public static void main(String[] args) {
 
+		// create a CdoUnit - some kind of configuration unit
 		CdoUnit cdoUnit = new CdoUnit("", "", URI.create("memory:///"),
 				Neo4jCdoProvider.class, getTypes(), ValidationMode.AUTO,
-				TransactionAttribute.REQUIRES, new Properties());
+				TransactionAttribute.MANDATORY, new Properties());
 
+		// create CdoManagerFactory using the previously create CdoUnit
 		CdoManagerFactory cdoManagerFactory = Cdo
 				.createCdoManagerFactory(cdoUnit);
+
+		// create a CdoManager
 		CdoManager cdoManager = cdoManagerFactory.createCdoManager();
 
+		// start a new transaction
+		cdoManager.currentTransaction().begin();
+
+		// fill the database with some sample data
 		Human janice = cdoManager.create(Human.class);
-		janice.setFirstname("Janice");
+		janice.setName("Janice");
 
 		Human matt = cdoManager.create(Human.class);
-		matt.setFirstname("Matt");
+		matt.setName("Matt");
 		Human suzie = cdoManager.create(Human.class);
-		suzie.setFirstname("Suzie");
+		suzie.setName("Suzie");
 		janice.getChildren().add(matt);
 		janice.getChildren().add(suzie);
 
 		Human cathy = cdoManager.create(Human.class);
-		cathy.setFirstname("Cathy");
+		cathy.setName("Cathy");
 		Human jack = cdoManager.create(Human.class);
-		jack.setFirstname("Jack");
+		jack.setName("Jack");
 		suzie.getChildren().add(cathy);
 		suzie.getChildren().add(jack);
 
 		Human tom = cdoManager.create(Human.class);
-		tom.setFirstname("Tom");
+		tom.setName("Tom");
 		Human cindy = cdoManager.create(Human.class);
-		cindy.setFirstname("Cindy");
+		cindy.setName("Cindy");
 		matt.getChildren().add(tom);
 		matt.getChildren().add(cindy);
 
+		// query the database for all grandchilds of Janice
 		Result<CompositeRowObject> result = cdoManager
 				.createQuery(
 						"MATCH (grandma)-->()-->(grandchild) WHERE grandma={janice} RETURN grandchild")
 				.withParameter("janice", janice).execute();
 		for (CompositeRowObject row : result) {
-			LOGGER.info(row.get("grandchild", Human.class).getFirstname());
+			// log the names of all grandchilds
+			LOGGER.info(row.get("grandchild", Human.class).getName());
 		}
+
+		// close / commit the transaction
+		cdoManager.currentTransaction().commit();
 
 	}
 
